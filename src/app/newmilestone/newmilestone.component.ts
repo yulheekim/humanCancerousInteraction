d676ffe66f654efec61a1488b673c75af6c9de07
San Lee <sanlee2017@u.northwestern.edu>
2018-02-14 23:00:04 -0600

    added readme

--- src/app/newmilestone/newmilestone.component.ts
+++ src/app/newmilestone/newmilestone.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MilestoneStoreService } from '../service/milestone-store.service';
 import { Milestone, MilestoneMaker } from '../model/milestone';
+import {isDate} from 'util';
 
 @Component({
   selector: 'app-newmilestone',
@@ -20,10 +21,22 @@ export class NewmilestoneComponent implements OnInit {
   public ms = new MilestoneMaker( this.title, this.days, this.startDate, this.endDate, 0 );
 
   saveMilestone() {
+    // date input should not be empy
+    // startdate must be prior to end date
+    if (this.startDate === '' || this.endDate === '' || this.endDate < this.startDate) {
+      alert('check your date');
+      return 0;
+    }
+    // date should be in date format
+    if ( !(isDate(this.startDate) && isDate(this.endDate)) ) {
+      alert('choose a date');
+      return 0;
+    }
+
     this.ms.name = this.title;
     this.ms.days = this.days;
-    this.ms.startDate = this.startDate.toString();
-    this.ms.endDate = this.endDate.toString();
+    this.ms.startDate = this.startDate;
+    this.ms.endDate = this.endDate;
     console.log(this.ms);
     this.msStore.addmilestone(this.ms);
     this.router.navigate(['/dashboard']);
