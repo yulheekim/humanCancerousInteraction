276b24e4ff98cf5f391be29e96246a145d1cf2e4
San Lee <sanlee2017@u.northwestern.edu>
2018-02-17 01:57:45 -0600

    able to sign in with Google account

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName: string;
+}
+
+@Injectable()
+export class AuthService {
+
+  user: Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router) {
+
+    // Get auth data and get firestore user document
+    this.user = this.afAuth.authState
+      .switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return Observable.of(null);
+        }
+      });
+  }
+
+  private oAuthLogin(provider) {
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then((credential) => {
+        this.router.navigate(['/dashboard']);
+        return this.updateUserDate(credential.user);
+      });
+  }
+
+  private updateUserDate(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    };
+
+    return userRef.set(data);
+  }
+
+  googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider);
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut()
+      .then(() => {
+        this.router.navigate(['/login']);
+      });
+  }
+}
